40c58811eaf8588e4f1e2d6e3fd1ad40223f2116
LT-Rascek <55886300+LT-Rascek@users.noreply.github.com>
2022-02-19 23:29:14 -0500

    Improve known error databases

--- BP/_bp_development_utilities/remove_known_errors.sh
+++ BP/_bp_development_utilities/remove_known_errors.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+#Combine the suppression error patterns
+cat _BP_known_vanilla_errors.info _BP_known_mod_errors.info > _bp_tmp_pattern_file.info
 #Greps the known errors, does the inverst match and replaces the error.log
-grep -Fvf _BP_known_errors.info error.log > tmp_error.log
+grep -Fvf _bp_tmp_pattern_file.info error.log > tmp_error.log
+#This will fail if file is locked (aka game is running)
 mv tmp_error.log error.log
