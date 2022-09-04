f566f5e7eabcec159835cfdc6d563e4b57ea2917
LT-Rascek <55886300+LT-Rascek@users.noreply.github.com>
2022-09-04 00:06:02 -0400

    Historical Notes

--- _user_documentation/build_documenation.bat
+++ _user_documentation/build_documenation.bat
@@ -2,6 +2,7 @@
 
 pandoc.exe "--columns=250" "-c" "pandoc.css" "-f" "mediawiki" "-t" "html5" "-s" "Faith.wiki" "-o" "Faith.html"
 pandoc.exe "--columns=250" "-c" "pandoc.css" "-f" "mediawiki" "-t" "html5" "-s" "Differences.wiki" "-o" "Differences.html"
+pandoc.exe "--columns=250" "-c" "pandoc.css" "-f" "mediawiki" "-t" "html5" "-s" "Historical_Notes.wiki" "-o" "Historical_Notes.html"
 pandoc.exe "--columns=250" "-c" "pandoc.css" "-f" "mediawiki" "-t" "html5" "-s" "Main_Page.wiki" "-o" "Main_Page.html"
 
 python3 "_merge_faith_table_cells.py"
