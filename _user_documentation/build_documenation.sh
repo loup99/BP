7328880db888bd9d5af7121a5f96205fbae26fee
LT-Rascek <55886300+LT-Rascek@users.noreply.github.com>
2022-09-04 21:10:41 -0400

    Traditions example

--- _user_documentation/build_documenation.sh
+++ _user_documentation/build_documenation.sh
@@ -1,6 +1,7 @@
 #Faith
 pandoc.exe --columns=250 -c pandoc.css -f mediawiki -t html5 -s Faith.wiki -o Faith.html
 pandoc.exe --columns=250 -c pandoc.css -f mediawiki -t html5 -s Culture.wiki -o Culture.html
+pandoc.exe --columns=250 -c pandoc.css -f mediawiki -t html5 -s Traditions.wiki -o Traditions.html
 pandoc.exe --columns=250 -c pandoc.css -f mediawiki -t html5 -s Differences.wiki -o Differences.html
 pandoc.exe --columns=250 -c pandoc.css -f mediawiki -t html5 -s Historical_Notes.wiki -o Historical_Notes.html
 pandoc.exe --columns=250 -c pandoc.css -f mediawiki -t html5 -s Main_Page.wiki -o Main_Page.html
